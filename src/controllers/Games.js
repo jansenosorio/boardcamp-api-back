a3abdb59918b920bb2bdeb91edcca63e26b97ab1
JansenOsorio <dev.jansen93@gmail.com>
2023-02-08 14:07:16 -0300

    feat: created conection on postgres database, fix: bug at connection to database

--- src/controllers/Games.js
+++ src/controllers/Games.js
@@ -1,18 +1,10 @@
+import { connection } from "../database/database.connection.js"
 
 export async function getGamesList(req, res) {
 
-    //test variable to test our program
-    const testeBody = {
-        id: 1,
-        name: 'Banco Imobiliário',
-        image: 'http://',
-        stockTotal: 3,
-        pricePerDay: 1500,
-    }
-
-
     try {
-        res.status(200).send(testeBody)
+        const { rows } = await connection.query('SELECT * FROM games');
+        res.send(rows)
     } catch (error) {
         res.status(500).send(error)
     }
--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -4,10 +4,8 @@ dotenv.config()
 
 const { Pool } = pg;
 
-const configDatabase = new Pool({
+export const connection = new Pool({
     connectionString: process.env.DATABASE_URL,
 });
 
-if (process.env.MODE === 'prod') configDatabase.ssl = true;
-
-export const db = new Pool(configDatabase)
+// if (process.env.MODE === 'prod') configDatabase.ssl = true;
