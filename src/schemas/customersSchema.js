eef0f98df3f604f8a50c27ac8e46599b00d1ad09
JansenOsorio <dev.jansen93@gmail.com>
2023-02-09 20:55:24 -0300

    fix: corrigido bug para verificar se o CPF tipo string possui apenas números

--- src/schemas/customersSchema.js
+++ src/schemas/customersSchema.js
@@ -3,6 +3,6 @@ import joi from 'joi'
 export const postRegisterCustomersSchema = joi.object({
     name: joi.string().required(),
     phone: joi.string().required().min(10).max(11),
-    cpf: joi.string().required().min(10).max(11),
+    cpf: joi.string().regex(/^\d{11}$/).required().min(10).max(11),
     birthday: joi.date().required()
 })
