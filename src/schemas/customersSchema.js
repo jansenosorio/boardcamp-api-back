5c08da0d25940e853314b1d13ffe316d87750f70
JansenOsorio <dev.jansen93@gmail.com>
2023-02-09 20:29:56 -0300

    fix: corrigido schema que recebia cpf com string, ao invés de número

--- src/schemas/customersSchema.js
+++ src/schemas/customersSchema.js
@@ -3,6 +3,6 @@ import joi from 'joi'
 export const postRegisterCustomersSchema = joi.object({
     name: joi.string().required(),
     phone: joi.string().required().min(10).max(11),
-    cpf: joi.string().required().min(10).max(11),
+    cpf: joi.number().required().min(10).max(11),
     birthday: joi.date().required()
 })
