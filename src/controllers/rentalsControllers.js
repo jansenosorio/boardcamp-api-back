60c5a79f7a410e644a7a454a2e79c25aedb534b7
JansenOsorio <dev.jansen93@gmail.com>
2023-02-10 17:46:47 -0300

    feat: concludes end point to create a new rental

--- src/controllers/rentalsControllers.js
+++ src/controllers/rentalsControllers.js
@@ -4,15 +4,17 @@ import dateFormat from "dateformat";
 export async function postRegisterRentals(req, res) {
     const { customerId, gameId, daysRented } = req.body
 
-    //Bibli to format dates
+    //To format date
     const newDate = dateFormat(new Date(), "yyyy-mm-dd")
 
     console.log(newDate)
     try {
-        const gamesInfo = await connection.query(`SELECT * FROM games WHERE id='${gameId}'`)
-        const customersInfo = await connection.query(`SELECT * FROM customers WHERE id='${customerId}'`)
-        const originalPrice = gamesInfo.rows[0].pricePerDay * daysRented
-        if (gamesInfo.rowCount < 1 || customersInfo.rowCount < 1) return res.sendStatus(400)
+        const gamesInfoById = await connection.query(`SELECT * FROM games WHERE id='${gameId}'`)
+        const customersInfoById = await connection.query(`SELECT * FROM customers WHERE id='${customerId}'`)
+        if (gamesInfoById.rowCount < 1 || customersInfoById.rowCount < 1) return res.sendStatus(400)
+        const rentalsInfo = await connection.query(`SELECT * FROM rentals WHERE "gameId"='${gameId}'`)
+        if (rentalsInfo.rowCount >= gamesInfoById.rows[0].stockTotal) return res.sendStatus(400)
+        const originalPrice = gamesInfoById.rows[0].pricePerDay * daysRented
         await connection.query(`INSERT INTO rentals ("customerId","gameId","rentDate","daysRented","returnDate","originalPrice","delayFee") VALUES ('${customerId}', '${gameId}', '${newDate}', '${daysRented}', null, '${originalPrice}', null)`)
         res.sendStatus(201)
     } catch (error) {
