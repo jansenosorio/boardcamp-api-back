d3b87446959e7e6bd85a5055e17782e29297ea1d
JansenOsorio <dev.jansen93@gmail.com>
2023-02-10 01:35:42 -0300

    feat: created end point to update customer register

--- src/controllers/customersControllers.js
+++ src/controllers/customersControllers.js
@@ -40,3 +40,19 @@ export async function postRegisterCustomers(req, res) {
         res.status(500).send(error)
     }
 }
+
+export async function updateCustomersRegisters(req, res) {
+    const { id } = req.params
+    const { name, phone, cpf, birthday } = req.body
+    const newId = Number(id)
+    const isDataValid = moment(birthday, 'YYYY-MM-DD', true).isValid()
+    try {
+        const { rowCount, rows } = await connection.query(`SELECT * FROM customers WHERE cpf='${cpf}'`)
+        if (!isDataValid) return res.sendStatus(400)
+        if (rowCount >= 1 && rows[0].id !== newId) return res.sendStatus(409)
+        await connection.query(`UPDATE customers SET name='${name}', phone='${phone}', cpf='${cpf}', birthday='${birthday}' WHERE id='${id}' `)
+        res.sendStatus(200)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
--- src/routes/customersRoutes.js
+++ src/routes/customersRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { getCustomers, postRegisterCustomers, getCustomersById } from '../controllers/customersControllers.js'
+import { getCustomers, postRegisterCustomers, getCustomersById, updateCustomersRegisters } from '../controllers/customersControllers.js'
 import { validateSchema } from '../middlewares/validateSchema.js'
 import { postRegisterCustomersSchema } from '../schemas/customersSchema.js'
 
@@ -9,7 +9,7 @@ const customerRouter = Router()
 customerRouter.get('/customers', getCustomers)
 customerRouter.get('/customers/:id', getCustomersById)
 customerRouter.post('/customers', validateSchema(postRegisterCustomersSchema), postRegisterCustomers)
-
+customerRouter.put('/customers/:id', validateSchema(postRegisterCustomersSchema), updateCustomersRegisters)
 
 
 export default customerRouter
