b47ce0691b3161ffe3acad2fa38970117469d9fa
JansenOsorio <dev.jansen93@gmail.com>
2023-02-10 21:31:25 -0300

    feat: created end point to send rentals array

--- src/controllers/rentalsControllers.js
+++ src/controllers/rentalsControllers.js
@@ -1,20 +1,65 @@
 import { connection } from "../database/database.connection.js";
 import dateFormat from "dateformat";
 
+
+export async function getRentals(req, res) {
+    try {
+        const { rows } = await connection.query(`
+        SELECT json_build_object(
+            'id', rentals.id,
+            'customerId', customers.id,
+            'gameId', games.id,
+            'rentDate', rentals."rentDate",
+            'daysRented', rentals."daysRented",
+            'returnDate', rentals."returnDate",
+            'originalPrice', rentals."originalPrice",
+            'delayFee', rentals."delayFee",
+            'customer', json_build_object(
+                'id', customers.id,
+                'name', customers.name
+            ),
+            'game', json_build_object(
+                'id', games.id,
+                'name', games.name 
+            )
+        ) 
+        FROM rentals
+        JOIN games
+        ON rentals."gameId" = games.id
+        JOIN customers
+        ON rentals."customerId" = customers.id
+        `)
+
+        const finalBody = rows.map(elm => {
+            return elm.json_build_object
+        })
+
+        res.send(finalBody)
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error)
+    }
+}
+
 export async function postRegisterRentals(req, res) {
     const { customerId, gameId, daysRented } = req.body
 
     //To format date
     const newDate = dateFormat(new Date(), "yyyy-mm-dd")
 
-    console.log(newDate)
     try {
+
+        //check if game id and customer id exists
         const gamesInfoById = await connection.query(`SELECT * FROM games WHERE id='${gameId}'`)
         const customersInfoById = await connection.query(`SELECT * FROM customers WHERE id='${customerId}'`)
         if (gamesInfoById.rowCount < 1 || customersInfoById.rowCount < 1) return res.sendStatus(400)
+
+        //check if game stock its not filled, to rental a game
         const rentalsInfo = await connection.query(`SELECT * FROM rentals WHERE "gameId"='${gameId}'`)
         if (rentalsInfo.rowCount >= gamesInfoById.rows[0].stockTotal) return res.sendStatus(400)
         const originalPrice = gamesInfoById.rows[0].pricePerDay * daysRented
+
+        //register at database new rental
         await connection.query(`INSERT INTO rentals ("customerId","gameId","rentDate","daysRented","returnDate","originalPrice","delayFee") VALUES ('${customerId}', '${gameId}', '${newDate}', '${daysRented}', null, '${originalPrice}', null)`)
         res.sendStatus(201)
     } catch (error) {
--- src/routes/rentalsRoutes.js
+++ src/routes/rentalsRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { postRegisterRentals } from "../controllers/rentalsControllers.js";
+import { getRentals, postRegisterRentals } from "../controllers/rentalsControllers.js";
 import { validateSchema } from "../middlewares/validateSchema.js";
 import { rentalsValidateSchema } from "../schemas/rentalsSchema.js";
 
 const rentalsRouter = Router()
 
 //Routes here
+rentalsRouter.get('/rentals', getRentals)
 rentalsRouter.post('/rentals', validateSchema(rentalsValidateSchema), postRegisterRentals)
 
 export default rentalsRouter
